e5ccbf570df2799523156e201cf4362a37b0b4b2
limweld <libradilla.limwel@gmail.com>
2018-07-04 17:22:57 +0800

    Initial MVVM Structure

---
+++ app/build/generated/source/buildConfig/debug/com/flexifinance/app/sim_loader/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.flexifinance.app.sim_loader;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.flexifinance.app.sim_loader";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "-debug";
+  // Fields from build type: debug
+  public static final boolean DEV_ENVIRONMENT = true;
+  public static final boolean LOGS = true;
+}
